9322e7e02d4f8b852656c93aa52e3970649c9f2c
LuisPavelA <agustin290208@gmail.com>
2022-07-26 14:40:23 +0100

    Added the ability to set the height and width of the website

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ program.command('preview')
     .description('Preview a website')
     .argument('<string>', 'website to preview')
     .option('-o, --output <string>', 'the output file', 'website_preview.png')
+    .option('-h, --height <number>', 'the height of the page', "1080")
+    .option('-w, --width <number>', 'the width of the page', "1920")
     .action(async (str, options) => {
-        const browser = await puppeteer.launch()
+        const browser = await puppeteer.launch({
+            defaultViewport: {
+                width: parseInt(options.width, 10),
+                height: parseInt(options.height, 10)
+            }
+        })
         const page = await browser.newPage()
         await page.goto(str)
+
         await page.screenshot({path: options.output})
 
         await browser.close()
